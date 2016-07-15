d03bfa7564a4790b66fa24cca441974ae040d2f8
JAlikhani <alikhani.j2al@gmai.com>
2016-07-15 10:39:31 +1000

    Initial Commit

---
+++ DynamicsSwiftiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicsSwiftiOS
+//
+//  Created by Jalal Alikhani on 15/07/2016.
+//  Copyright © 2016 Jalal Alikhani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
